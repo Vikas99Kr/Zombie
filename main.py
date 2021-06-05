c826c3069a55f226f878bc5dd496702c5bb0c6db
Vikas Kumar <vikas@github.com>
2021-06-05 16:09:59 +0530

    input system

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ display = GameView(600, 500, "MyGame")
 def render(screen):
     if Input.get_key_pressed('enter'):
         print("You pressed enter")
+    if Input.get_mouse_button_down(0):
+        print("Left Mouse Button is down")
+    if Input.get_mosue_button_up(0):
+        print("Left Mouse Button is released")
+    if Input.get_mouse_button_down(2):
+        print("Right mouse button is down")
+    if Input.get_mosue_button_up(2):
+        print("Right Mouse button is released")
+
 
 
 display.run(render)
